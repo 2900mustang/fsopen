c4947d2aed62cb6cda79ccf78d645ae3d325e785
NITGO_SBR <Honors@ORCA.fios-router.home>
2019-11-06 23:52:09 -0500

    added weather feature to Countries

--- part2/find-countries/src/App.js
+++ part2/find-countries/src/App.js
@@ -1,50 +1,30 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import Filter from './components/Filter'
+import React, {useEffect, useState} from 'react';
+import countryServices from './services/countries'
+import Search from './components/Search'
+import Countries from './components/Countries'
 const log = console.log
 
 const App = () => {
-	const [allCountries, setAllCountries] = useState([])
-	const [displayCountries, setDisplayCountries] = useState(allCountries)
-	const [query, setQuery] = useState('')
+	const [countries, setCountries] = useState([])
+	const [search, setSearch] = useState('chez')
 
 	const hook = () => {
-		axios.get('https://restcountries.eu/rest/v2/all').then(res => {
-			setAllCountries(res.data)
-		})
-		// axios.get('https://restcountries.eu/rest/v2/name/hin').then(res => {
-		// 	log(res.data)
-		// })
+		countryServices
+			.getAll().then(allCountries => setCountries(allCountries))
 	}
-	
+
 	useEffect(hook, [])
 
-	const filteredResult = query ? allCountries.filter(country => 
-		country.name.toLowerCase().includes(query.toLowerCase())
-	) : allCountries
+	const countriesToShow = search.length ? countries.filter(c => c.name.toLowerCase().includes(search.toLowerCase())) : countries
 
-	if (filteredResult.length !== allCountries.length) {
-		if (filteredResult.length < 10) {
-			setDisplayCountries(filteredResult)
-		} else {
-			setDisplayCountries('Too many matches, specify another filter')
-		}
-	}
-
-	log(displayCountries)
-	const display = () => {
-		displayCountries.map(country => 
-			<li>{country.name}</li>)
-	}
-
-	const handleQueryChange = (event) => {
-		setQuery(event.target.value)
-	}
+	const handleChange = event => setSearch(event.target.value)
 
 	return (
 		<div>
-			<Filter query={query} onQueryChange={handleQueryChange} />
-			<ul>{display()}</ul>
+			<div>
+				<Search value={search} onChange={handleChange} />
+				<Countries countries={countriesToShow} handleClick={(e) => setSearch(e)} />
+			</div>
 		</div>
 	);
 };
